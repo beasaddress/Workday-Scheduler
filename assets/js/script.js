6a0f596babbe385b02786847911524ffa63f204a
Beatriz Sandoval <beasaddress@gmail.com>
2023-05-02 17:06:15 -0700

    removed comments from starter code, removed comments that were fault code from prior, and removed random empty spaces

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,6 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-//creating two function that will use dayjs to display the current time and day in the header
-
-
-
-
-  
-//this is going to wait until the DOM loads before executing the function
-
 $(function () {
     //formatting the time to just the H, so that the function can determine if the time block is past, present, or future. 
     //Multiple functions in this file will be constantly referncing this 'H' since we put it in a varaible called currentHour.
@@ -69,24 +61,5 @@ $(function () {
      changeColor();
 
      setInterval(updateClock, 1000);
-     
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
   });
   
